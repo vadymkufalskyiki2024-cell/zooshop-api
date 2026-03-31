193e13109e33ef92dfd205de467e1f4f39eedfa3
Vadym Kufalskyi <vadym.kufalskyi.ki.2024@lpnu.ua>
2026-03-31 16:17:24 +0300

    Introduce CI error

--- app.py
+++ app.py
@@ -1,116 +1,30 @@
-# from flask import Flask, jsonify, request
-# import mysql.connector
-
-# app = Flask(__name__)
-
-# # Підключення до бази даних
-# db = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password="root",       # встав свій пароль
-#     database="petstoreTrue" # заміни на назву твоєї бази
-# )
-
-# cursor = db.cursor(dictionary=True)
-
-# # GET - отримати всіх клієнтів
-# @app.route('/customers', methods=['GET'])
-# def get_customers():
-#     cursor.execute("SELECT * FROM customers")
-#     results = cursor.fetchall()
-#     return jsonify(results)
-
-# # GET - історія покупок клієнта
-# @app.route('/purchase_history/<int:customer_id>', methods=['GET'])
-# def get_purchase_history(customer_id):
-#     query = """
-#     SELECT ph.history_id, p.product_name, ph.quantity, ph.purchase_date
-#     FROM purchase_history ph
-#     JOIN products p USING(product_id)
-#     WHERE ph.customer_id = %s
-#     """
-#     cursor.execute(query, (customer_id,))
-#     results = cursor.fetchall()
-#     return jsonify(results)
-
-# # POST - додати нову покупку
-# @app.route('/purchase_history', methods=['POST'])
-# def add_purchase():
-#     data = request.json
-#     query = """
-#     INSERT INTO purchase_history (customer_id, product_id, quantity, purchase_date)
-#     VALUES (%s, %s, %s, %s)
-#     """
-#     cursor.execute(query, (
-#         data['customer_id'],
-#         data['product_id'],
-#         data['quantity'],
-#         data['purchase_date']
-#     ))
-#     db.commit()
-#     return jsonify({"message": "Purchase added successfully"}), 201
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=3000)
-
-
 from flask import Flask, jsonify, request
-import mysql.connector
 
 app = Flask(__name__)
-print(abcd)
-# Підключення до бази даних
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",       # твій пароль
-    database="petstoreTrue" # назва твоєї бази
-)
+
+# Фейкове підключення для CI
+class FakeDB:
+    def cursor(self, dictionary=False):
+        return self
+    def execute(self, query, params=None):
+        pass
+    def fetchall(self):
+        return []
+    def commit(self):
+        pass
+
+db = FakeDB()
 cursor = db.cursor(dictionary=True)
 
-
-# --------------------
-# GET - отримати всіх клієнтів
-# --------------------
 @app.route('/customers', methods=['GET'])
 def get_customers():
-    cursor.sfsrfvsdrfsfesfse("SELECT * FROM customers")
+    cursor.execute("SELECT * FROM customers")
     results = cursor.fetchall()
     return jsonify(results)
-asfdcesf
-# --------------------
-# POST - додати нового клієнта
-# --------------------
-@app.route('/customers', methods=['POST'])
-def add_customer():
-    data = reqfrsdgfvrsgvsrdvgfsdvfrdgrdgfvdfgvuest.json
-    query = "INSERT INTO customers (customer_name, phone, email) VALUES (%s, %s, %s)"
-    cursor.execute(query, (data['customer_name'], data['phone'], data['email']))
-    db.commit()
-    return jsonify({"message": "Customer added", "id": cursor.lastrowid}), 201
 
-# --------------------
-# PUT - оновити клієнта
-# --------------------
-@app.route('/customers/<int:id>', methods=['PUT'])
-def update_curdgfvfdv fdbzfdnbdxbxdbgfdxgbstomer(id):
-    data = request.json
-    query = "UPDATE customers SET customer_name=%s, phone=%s, email=%s WHERE customer_id=%s"
-    cursor.execute(query, (data['customer_name'], data['phone'], data['email'], id))
-    db.commit()zdfbnzdffbhzdrfxbhdfcbhzdf
-    return jsonify({"message": "Customer updated"})
+# Тільки для локального запуску
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
 
-# --------------------
-# DELETE - видалити клієнта
-# --------------------
-@app.route('/customers/<int:id>', methods=['DELETE'])
-def delete_custdbfdxhbrstfhnbdfbhdfbhomer(id):
-    query = "DELETE FROM customers WHERE customer_id=%s"
-    cursor.execute(query, (id,))
-    db.commit()
-    return jsonify({"message": "Customer deleted"})
-
-# if __name__ == '__main__' and False:  # не запускати сервер у CI
-#     app.run(debug=True, port=3000)
-
-print(abcd)  # <-- тут помилка спрацює
+# Навмисна помилка для демонстрації CI
+print(abcd)
